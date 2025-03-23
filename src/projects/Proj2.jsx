9ebcda8b722191d9f4357a90fefbcd4251491cbc
HarishVijendiran97 <harish.vijendiran@gmail.com>
2025-03-23 13:49:59 +0530

    deploy 3

--- src/projects/Proj2.jsx
+++ src/projects/Proj2.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import proj2 from '../assets/proj2.png'
 
 //! Portfolio 
 
 const Proj2 = () => {
+
+  const [menu, setMenu] = useState(false)
+
+  const handleClick = () => {
+    setMenu(!menu)
+    console.log(menu)
+  }
+
   return (
-    <div className='transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center h-[200px] bg-cover relative'>
+    <div onClick={handleClick} className='transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center h-[200px] bg-cover relative'>
       <img src={proj2} alt="" />
-      <div className='opacity-0 group-hover:opacity-90 bg-[gray]/60 absolute inset-0 flex flex-col justify-center items-center'>
+      
+      {/* Menu for larger screens (shown on hover) */}
+      <div className="hidden md:flex opacity-0 group-hover:opacity-100 bg-[gray]/60 absolute inset-0 flex-col justify-center items-center transition-opacity duration-300">
+        <span className="text-2xl font-bold text-white tracking-wider"></span>
+        <div className="pt-8 text-center">
+          <a href="https://github.com/HarishVijendiran1997/Portfolio" target="_blank">
+            <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Github</button>
+          </a>
+          <a href="https://portfolio-online-eight.vercel.app/" target="_blank">
+            <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Live</button>
+          </a>
+        </div>
+      </div>
+
+      {/* Menu for smaller screens (shown on click) */}
+      {menu && (<div className='opacity-100 bg-[gray]/60 absolute inset-0 flex flex-col justify-center items-center'>
         <span className='text-2xl font-bold text-white tracking-wider'></span>
         <div className='pt-8 text-center'>
           <a href='https://github.com/HarishVijendiran1997/Portfolio' target='_blank'>
@@ -17,7 +40,7 @@ const Proj2 = () => {
             <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Live</button>
           </a>
         </div>
-      </div>
+      </div>)}
     </div>
   )
 }
